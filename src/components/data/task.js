c472117950ba00d5b10913e7e4bdf038089dcfe7
Alfonso-programador <vasquesoto@gmail.com>
2023-01-01 19:08:59 -0500

    first commit

---
+++ src/components/data/task.js
@@ -0,0 +1,18 @@
+export const tasks = [
+    {
+        id: 0,
+        title: "mi primer tarea",
+        description: "Esta es mi primera tarea"
+    },
+    {
+        id: 1,
+        title: "mi segunda tarea",
+        description: "Esta es mi segunda tarea"
+    },
+    {
+        id: 2,
+        title: "mi tercera tarea",
+        description: "Esta es mi tercera tarea"
+    }
+
+]
